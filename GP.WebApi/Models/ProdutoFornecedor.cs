f0b92011d704ad61764b1e7b0406a851c16c25a1
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-05 20:45:58 -0300

    add: Models

--- GP.WebApi/Models/ProdutoFornecedor.cs
+++ GP.WebApi/Models/ProdutoFornecedor.cs
@@ -2,6 +2,14 @@ namespace GP.WebApi.Models
 {
     public class ProdutoFornecedor
     {
+        public ProdutoFornecedor() { }
+
+        public ProdutoFornecedor(int produtoId, int fornecedorId)
+        {
+            this.ProdutoId = produtoId;
+            this.FornecedorId = fornecedorId;
+
+        }
         public int ProdutoId { get; set; }
         public Produto Produto { get; set; }
         public int FornecedorId { get; set; }
