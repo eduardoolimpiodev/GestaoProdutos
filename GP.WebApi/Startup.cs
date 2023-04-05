75ca1eef2f1157c8cbd50bff6fa7b03f28031eca
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-05 18:02:11 -0300

    add: Startup

--- GP.WebApi/Startup.cs
+++ GP.WebApi/Startup.cs
@@ -36,11 +36,11 @@ namespace GP.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
