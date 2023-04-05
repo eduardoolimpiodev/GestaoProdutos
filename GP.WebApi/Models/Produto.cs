68d7858d6ac868bbae2fd4e5c9415d785fbc2344
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-05 20:35:24 -0300

    add: Models Produto

---
+++ GP.WebApi/Models/Produto.cs
@@ -0,0 +1,25 @@
+using System;
+namespace GP.WebApi.Models
+{
+    public class Produto
+    {
+        public Produto() { }
+
+        public Produto(int id, string nome, string descricao, bool situacao, DateTime dataFabricacao, DateTime dataValidade)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Descricao = descricao;
+            this.Situacao = situacao;
+            this.DataFabricacao = dataFabricacao;
+            this.DataValidade = dataValidade;
+
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Situacao { get; set; }
+        public DateTime DataFabricacao { get; set; }
+        public DateTime DataValidade { get; set; }
+    }
+}
