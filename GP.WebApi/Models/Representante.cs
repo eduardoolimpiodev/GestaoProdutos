838b2bc07249249ccdb229bf93f040ca5b3ba1c7
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-05 20:48:35 -0300

    add: Models

--- GP.WebApi/Models/Representante.cs
+++ GP.WebApi/Models/Representante.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace GP.WebApi.Models
 {
     public class Representante
     {
-        
+        public Representante() { }
+
+        public Representante(int id, string nome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public IEnumerable<Fornecedor> Fornecedores { get; set; }
     }
 }
