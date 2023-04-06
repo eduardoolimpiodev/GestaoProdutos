86fad6e96432e55ff6cc771a674a5f449051c764
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-06 19:39:57 -0300

    add: Interfaces

--- GP.WebApi/Data/IRepository.cs
+++ GP.WebApi/Data/IRepository.cs
@@ -1,3 +1,5 @@
+using GP.WebApi.Models;
+
 namespace GP.WebApi.Data
 {
     public interface IRepository
@@ -8,7 +10,21 @@ namespace GP.WebApi.Data
 
         void Delete<T>(T entity) where T : class;
 
-
         bool SaveChanges();
+
+        //Produtos
+        Produto[] GetAllProdutos(bool includeRepresentante = false);
+
+        Produto[] GetAllProdutosByFornecedorId(int fornecedorId, bool includeRepresentante = false);
+
+        Produto GetProdutoById(int produtoId, bool includeRepresentante = false);
+
+        //Representante
+        Representante[] GetAllRepresentantes(bool includeFornecedores = false);
+
+        Representante[] GetAllRepresentantesByFornecedorId(int fornecedorId, bool includeProdutos = false);
+
+        Representante GetRepresentanteById(int representanteId, bool includeRepresentante = false);
+
     }
 }
